470fe8d468f854d6d909a88141b2595e97cb85a6
Obvman <obvman@gmail.com>
2017-02-25 02:09:56 +1100

    CPUMeasure documentation

--- RetroGraph/CPUMeasure.h
+++ RetroGraph/CPUMeasure.h
@@ -15,20 +15,32 @@ public:
     CPUMeasure(Window* w, int32_t graphWidth, int32_t graphHeight);
     ~CPUMeasure();
 
+    /* Updates the total system's CPU usage statistics */
     void update();
+
+    /* Draws the components of the measure */
     void draw() const;
+
+    /* Draws the total system uptime text */
     void drawUptime() const;
+
+    /* Draws the CPU usage graph */
     void drawGraphBox() const;
+
+    /* Returns the current system CPU load as a percentage */
     float getCPULoad();
 
-    std::string getUptimeStr() const;
 private:
+    /* Returns string containing the current system uptime in
+       dd:hh:mm:ss format */
+    std::string getUptimeStr() const;
+
+    /* Calculates the total CPU load with the given tick information */
     float calculateCPULoad(uint64_t idleTicks, uint64_t totalTicks);
 
     Window* m_parentWindow;
     size_t dataSize; // max number of usage percentages to store
     std::vector<float> m_usageData;
-
     std::chrono::milliseconds m_uptime;
 
     // Rendering members
