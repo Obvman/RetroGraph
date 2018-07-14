1d988a958ffc0fb5ef897c27f5ddcae8ccd82316
obvman <obvman@gmail.com>
2018-07-14 14:07:58 +1000

    Created batch script to build and package the program in 'Package\' dir

---
+++ build.bat
@@ -0,0 +1,73 @@
+@rem ECHO off
+
+@rem Build Process:
+@rem 1. Clean the package directory
+@rem 2. Make package directory (Package)
+@rem 3. Build the target. Release x64 by default.
+@rem 4. From output Directory:
+@rem     Copy .dll files
+@rem     Copy RetroGraph.exe
+@rem 5. Copy resources to output dir.
+@rem     Rename default_config.ini to config.ini
+
+@SET PKGDIR=Package\
+@SET RSRCDIR=%PKGDIR%resources\
+@SET BINDIR=%PKGDIR%bin\
+@SET DEVENVCOM="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.com"
+
+@IF [%1]==[rebuild] GOTO :build
+@SET ACTION=-build
+GOTO :start_build
+
+:build
+@SET ACTION=-rebuild
+GOTO :start_build
+
+:start_build
+@ECHO.
+@ECHO ========================================================================
+@ECHO Compiling
+@ECHO ========================================================================
+
+@rem Compile project.
+%DEVENVCOM% RetroGraph.sln %ACTION% "Release|x64" || goto :error
+
+@ECHO.
+@ECHO ========================================================================
+@ECHO Copying bin directory files
+@ECHO ========================================================================
+
+@RMDIR /S /Q "Package"
+@MKDIR "Package" || goto :error
+
+@COPY "RetroGraph\InstallNotes.txt" %PKGDIR% >NUL || goto :error
+
+@XCOPY /Q "bin\Releasex64\freeglut.dll" %BINDIR% >NUL || goto :error
+@XCOPY /Q "bin\Releasex64\glew32.dll" %BINDIR% >NUL || goto :error
+@XCOPY /Q "bin\Releasex64\GetCoreTempInfo.dll" %BINDIR% >NUL || goto :error
+@XCOPY /Q "bin\Releasex64\RetroGraph.exe" %BINDIR% >NUL || goto :error
+
+@ECHO.
+@ECHO ========================================================================
+@ECHO Copying resource directory files
+@ECHO ========================================================================
+
+@XCOPY /Q /E /I "RetroGraph\resources\fonts" "Package\resources\fonts" >NUL || goto :error
+@XCOPY /Q /E /I "RetroGraph\resources\shaders" "Package\resources\shaders" >NUL || goto :error
+@COPY  "RetroGraph\resources\default_config.ini" "Package\resources\config.ini" >NUL || goto :error
+
+:success
+@ECHO.
+@ECHO ========================================================================
+@ECHO Successfully built package.
+@ECHO ========================================================================
+EXIT /B 0
+
+:error
+@ECHO.
+@ECHO ========================================================================
+ECHO "Failed to package. Exiting."
+@ECHO ========================================================================
+EXIT /B %errorlevel%
+
+PAUSE
